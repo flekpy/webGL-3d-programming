352e1026502e4bb99c09b4ab727dd9e5e7c3a8ce
flekpy <flekpy@gmail.com>
2023-10-30 15:26:53 +0800

    add point on click

---
+++ clicked-point/clickedPoint.js
@@ -0,0 +1,71 @@
+// Simple webGL-program point
+// add dynamic values from js and set point on click 
+
+// верхний шейдер
+const VSHADER_SOURCE = 'attribute vec4 a_Position;\n' +
+  'void main() {\n' + 
+      'gl_Position = a_Position;\n' +
+      'gl_PointSize = 10.0;\n' + 
+  '}'
+
+// фрагментный шейдер
+const FSHADER_SOURCE = 'void main() { gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); }'  
+
+let gl
+function main() {
+  const canvas = document.getElementById('webgl')
+  if (!getWebGLContext(canvas)) {
+    console.log('failed to get the render context webGL')
+    return
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('failed init shaders')
+    return
+  }
+
+  // получаем ссылку на переменную-атрибут
+  const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+  if (a_Position < 0) {
+    console.log('failed to get the storage location of a_Position')
+    return
+  }
+
+  // регистрируем функцию обработчик для вызова по щелчку
+  canvas.onmousedown = (event) => click(event, gl, canvas, a_Position)
+
+  // указываем цвет для очистки <canvas>
+  gl.clearColor(0.0, 0.0,  0.0, 1.0)
+
+  // очищяем <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+const g_points = []
+function click(event, gl, canvas, a_Position) {
+  let x = event.clientX
+  let y = event.clientY
+  let rect = event.target.getBoundingClientRect()
+
+  x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2)
+  y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2)
+
+  // сохраняем координаты в массиве g_points
+  g_points.push(x)
+  g_points.push(y)
+
+  // очищяем <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  // очищяем <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  let len = g_points.length
+  for (let i = 0; i < len; i += 2) {
+    // передаём координаты щелчка в переменную a_Position
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0)
+
+    // рисуем точку
+    gl.drawArrays(gl.POINTS, 0, 1)
+  }
+}
--- hello-point2/helloPoint2.js
+++ hello-point2/helloPoint2.js
@@ -3,9 +3,10 @@
 
 // верхний шейдер
 const VSHADER_SOURCE = 'attribute vec4 a_Position;\n' +
+  'attribute float a_PointSize; \n' +
   'void main() {\n' + 
       'gl_Position = a_Position;\n' +
-      'gl_PointSize = 10.0;\n' + 
+      'gl_PointSize = a_PointSize;\n' + 
   '}'
 
 // фрагментный шейдер
@@ -26,13 +27,15 @@ function main() {
 
   // получаем ссылку на переменную-атрибут
   const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-  if (a_Position < 0) {
+  const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
+  if (a_Position < 0 && a_PointSize < 0) {
     console.log('failed to get the storage location of a_Position')
     return
   }
 
   // сохраняем координаты в переменной-атрибуте
   gl.vertexAttrib3f(a_Position, -0.5, -0.5, 0.0)
+  gl.vertexAttrib1f(a_PointSize, 50.0)
 
   // указываем цвет для очистки <canvas>
   gl.clearColor(0.0, 0.0,  0.0, 1.0)
